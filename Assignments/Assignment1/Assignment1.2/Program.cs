30d7edbee0baf4d9d1c9a79f28a8af80d98bc35a
PrathameshDeshpande6546 <prathameshdeshpande68@gmail.com>
2024-12-26 16:10:48 +0530

    Assignment 1

---
+++ Assignments/Assignment1/Assignment1.2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1._2
+{
+    
+        
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int add(int x, int y)
+            {
+                return x + y;
+            }
+
+            int sub(int x, int y)
+            {
+                return x - y;
+            }
+
+            int mul(int x, int y)
+            {
+                return x * y;
+            }
+            int div(int x, int y)
+            {
+                return x / y;
+            }
+
+            Console.WriteLine("1.Addition");
+            Console.WriteLine("2.Subtract");
+            Console.WriteLine("3.Multiply");
+            Console.WriteLine("4.Divide");
+            Console.WriteLine("Enter your choice");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Calculator");
+            Console.WriteLine("Enter first Number");
+           int a= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number");
+           int b= Convert.ToInt32(Console.ReadLine());
+
+           
+
+
+
+
+           
+
+            switch (c)
+            {
+                case 1: Console.WriteLine(add(Convert.ToInt32(a), Convert.ToInt32(b)));
+                    break;
+                case 2:
+                    Console.WriteLine(sub(Convert.ToInt32(a), Convert.ToInt32(b)));
+                    break;
+                case 3:
+                    Console.WriteLine(mul(Convert.ToInt32(a), Convert.ToInt32(b)));
+                    break;
+                case 4:
+                    Console.WriteLine(div(Convert.ToInt32(a), Convert.ToInt32(b)));
+                    break;
+
+            }
+
+           
+        }
+    }
+}
---
+++ Assignments/Assignment1/Assignment1.3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int add(int x, int y)
+            {
+                return x + y;
+            }
+
+            int sub(int x, int y)
+            {
+                return x - y;
+            }
+
+            int mul(int x, int y)
+            {
+                return x * y;
+            }
+            int div(int x, int y)
+            {
+                return x / y;
+            }
+            while (true)
+            {
+
+                Console.WriteLine("Calculator");
+                Console.WriteLine("**************");
+                Console.WriteLine("1.Addition\n2.Subtract\n3.Multiply\n4.Divide\n5.Exit");
+              
+                Console.WriteLine("Enter your choice");
+                int c = Convert.ToInt32(Console.ReadLine());
+
+
+                switch (c)
+                {
+                    case 1:
+                        Console.WriteLine("Enter first Number");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Second Number");
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(add(Convert.ToInt32(a), Convert.ToInt32(b)));
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter first Number");
+                        int p= Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Second Number");
+                        int q = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(sub(Convert.ToInt32(p), Convert.ToInt32(q)));
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter first Number");
+                        int x = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Second Number");
+                        int y = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(mul(Convert.ToInt32(x), Convert.ToInt32(y)));
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter first Number");
+                        int l = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Second Number");
+                        int m = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(div(Convert.ToInt32(l), Convert.ToInt32(m)));
+                        break;
+                    case 5:return;
+                    default:Console.WriteLine("Invalid Input");
+                        break;
+
+                }
+            }
+
+
+        }
+    }
+}
