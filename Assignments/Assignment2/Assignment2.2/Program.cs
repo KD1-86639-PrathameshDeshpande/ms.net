3f78a9d2981473fa88b912edaa5be8c2151cce0f
PrathameshDeshpande6546 <prathameshdeshpande68@gmail.com>
2024-12-27 17:29:33 +0530

    Assignment 2

---
+++ Assignments/Assignment2/Assignment2.2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2._2
+{
+    internal class Program
+    {
+
+
+
+        public static void CreateArray( Student[] students)
+        {
+
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i] = new Student();
+                students[i].Accept();
+            }
+
+
+        }
+        public static void DisplayStudents( Student[] students)
+        {
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i].GetDetails();
+                Console.WriteLine();
+            }
+        }
+
+
+        public static Student[] ReverseArray(Student[] students)
+        {
+            Student[] newArray = new Student[students.Length];
+           
+            for (int i = 0,j=students.Length-1 ; i < students.Length && j >= 0; i++,j--)
+            {
+                newArray[i] = students[j];
+            }
+            return newArray;
+            
+
+        }
+      
+
+
+
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            
+
+            Console.WriteLine("Enter Array size");
+            int size =int.Parse( Console.ReadLine());
+            Student[] students = new Student[size];
+            CreateArray (students);
+            Console.WriteLine();
+            DisplayStudents(students);
+            Console.WriteLine();
+
+            
+
+           
+            Console.WriteLine();
+            Console.WriteLine("Reversed Array");
+            DisplayStudents(ReverseArray(students));
+
+
+        }
+    }
+}
---
+++ Assignments/Assignment2/Assignment2.2/Student.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2._2
+{
+    internal class Student
+    {
+
+     
+            private String _Name;
+            private int _Age;
+            private double _Marks;
+            private char _Div;
+            private int _Std;
+            private bool _Gender;
+
+            public bool Gender
+            {
+                get { return _Gender; }
+                set { _Gender = value; }
+            }
+
+
+            public int Std
+            {
+                get { return _Std; }
+                set { _Std = value; }
+            }
+
+
+            public char Div
+            {
+                get { return _Div; }
+                set { _Div = value; }
+            }
+
+
+            public double Marks
+            {
+                get { return _Marks; }
+                set { _Marks = value; }
+            }
+
+
+            public int Age
+            {
+                get { return _Age; }
+                set { _Age = value; }
+            }
+
+
+
+
+            public String Name
+            {
+                get { return _Name; }
+                set { _Name = value; }
+            }
+        public Student()
+        {
+            
+        }
+
+
+
+        public Student(string name, int age, double marks, char div, int std, bool gender)
+            {
+                _Name = name;
+                _Age = age;
+                _Marks = marks;
+                _Div = div;
+                _Std = std;
+                _Gender = gender;
+            }
+            public void Accept()
+            {
+                Console.WriteLine("Enter Student Information");
+                Console.WriteLine("Enter Name");
+                _Name = Console.ReadLine();
+                Console.WriteLine("Enter Age");
+                _Age = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Div");
+                _Div = char.Parse(Console.ReadLine());
+
+
+                bool flag = true;
+
+                while (flag)
+                {
+                    Console.WriteLine("Enter Gender (1.Male, 2.Female)");
+                    int gender = int.Parse(Console.ReadLine());
+                    switch (gender)
+                    {
+                        case 1:
+                            _Gender = true;
+                            flag = false;
+                            break;
+
+                        case 2:
+                            _Gender = false;
+                            flag = false;
+                            break;
+                        default:
+                            Console.WriteLine("Please enter only (M/F)");
+                            break;
+
+                    }
+                }
+
+
+
+                Console.WriteLine("Enter Standard");
+                _Std = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Marks");
+                _Marks = int.Parse(Console.ReadLine());
+
+
+            }
+
+            public void GetDetails()
+            {
+                Console.WriteLine("Student Information");
+                Console.WriteLine("Name: " + _Name);
+                Console.WriteLine("Age:" + _Age);
+                Console.WriteLine("Div:" + _Div);
+                if (_Gender)
+                {
+                    Console.WriteLine("Gender: Male");
+                }
+                else
+                {
+                    Console.WriteLine("Gender: Female");
+                }
+                Console.WriteLine("Std: " + _Std);
+                Console.WriteLine("Marks: " + _Marks);
+            }
+
+
+      
+
+           
+            
+
+
+        }
+    }
+
